aaf7be44edff16e7b5401827d3f14c4a8541666c
Damith C. Rajapakse <damith@gmail.com>
2016-09-03 17:13:23 +0800

    Remove TextUi and related tests

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -14,6 +14,9 @@ import java.io.IOException;
  */
 public class Gui {
 
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
     public static final int INITIAL_WINDOW_WIDTH = 800;
     public static final int INITIAL_WINDOW_HEIGHT = 600;
     private final Logic logic;
