071d1d5d469877d74e116db91acd93d5b5a1c328
Tjong Anthony <tjonganthony@gmail.com>
2016-09-14 01:59:43 +0800

    Prevent not mutating command to mutate storage file

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -85,7 +85,9 @@ public class Logic {
     private CommandResult execute(Command command) throws Exception {
         command.setData(addressBook, lastShownList);
         CommandResult result = command.execute();
-        storage.save(addressBook);
+        if (command.isMutating()) {
+            storage.save(addressBook);
+        }
         return result;
     }
 
