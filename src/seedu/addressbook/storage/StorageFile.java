a5da1e384fd0127e9fa1c44cdab1421a4c7699cd
qhng <qhng@u.nus.edu>
2016-09-29 12:47:01 +0800

    Apply Dependency Inversion Principle

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -15,7 +15,7 @@ import java.nio.file.Paths;
 /**
  * Represents the file used to store address book data.
  */
-public class StorageFile {
+public class StorageFile extends Storage {
 
     /** Default file path used if the user doesn't provide the file name. */
     public static final String DEFAULT_STORAGE_FILEPATH = "addressbook.txt";
@@ -27,18 +27,8 @@ public class StorageFile {
     /**
      * Signals that the given file path does not fulfill the storage filepath constraints.
      */
-    public static class InvalidStorageFilePathException extends IllegalValueException {
+    public static class InvalidStorageFilePathException extends StorageException {
         public InvalidStorageFilePathException(String message) {
-            super(message);
-        }
-    }
-
-    /**
-     * Signals that some error has occured while trying to convert and read/write data between the application
-     * and the storage file.
-     */
-    public static class StorageOperationException extends Exception {
-        public StorageOperationException(String message) {
             super(message);
         }
     }
@@ -50,14 +40,14 @@ public class StorageFile {
     /**
      * @throws InvalidStorageFilePathException if the default path is invalid
      */
-    public StorageFile() throws InvalidStorageFilePathException {
+    public StorageFile() throws StorageException {
         this(DEFAULT_STORAGE_FILEPATH);
     }
 
     /**
      * @throws InvalidStorageFilePathException if the given file path is invalid
      */
-    public StorageFile(String filePath) throws InvalidStorageFilePathException {
+    public StorageFile(String filePath) throws StorageException {
         try {
             jaxbContext = JAXBContext.newInstance(AdaptedAddressBook.class);
         } catch (JAXBException jaxbe) {
