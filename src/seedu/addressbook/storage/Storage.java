b3341fef4fbd3eb1c29aa415490b7bdeb0cc8196
MYPC\Fifer <alwinsonauyong@hotmail.com>
2017-02-28 18:49:07 +0800

    LO-DI Ready to Review

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -7,7 +7,7 @@ import seedu.addressbook.storage.StorageFile.StorageOperationException;
  */
 public abstract class Storage {
     
-    public abstract void save(AddressBook addressBook) throws StorageOperationException;
-    public abstract AddressBook load() throws StorageOperationException;
+    public abstract void save(AddressBook addressBook) throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;
+    public abstract AddressBook load() throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;
     public abstract String getPath();
 }
