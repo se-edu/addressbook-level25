c08dc86c50b5ddafe84d081cfa43c29a8802ec6b
Xie Jihui <xjh125470939@gmail.com>
2017-03-02 20:11:46 +0800

    Use the AddCommandTest in AddressBook level 2 Modify Address class to ensure that empty address is not allowed

--- test/java/seedu/addressbook/util/TestUtil.java
+++ test/java/seedu/addressbook/util/TestUtil.java
@@ -1,11 +1,21 @@
 package seedu.addressbook.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.tag.UniqueTagList;
 
 public class TestUtil {
     /**
@@ -16,5 +26,15 @@ public class TestUtil {
         List<String> list1 = Files.readAllLines(path1, Charset.defaultCharset());
         List<String> list2 = Files.readAllLines(path2, Charset.defaultCharset());
         assertEquals(String.join("\n", list1), String.join("\n", list2));
+    }
+    
+    public static Person generateTestPerson() {
+        try {
+            return new Person(new Name(Name.EXAMPLE), new Phone(Phone.EXAMPLE, false),
+                    new Email(Email.EXAMPLE, true), new Address(Address.EXAMPLE, false), new UniqueTagList());
+        } catch (IllegalValueException e) {
+            fail("test person data should be valid by definition");
+            return null;
+        }
     }
 }
