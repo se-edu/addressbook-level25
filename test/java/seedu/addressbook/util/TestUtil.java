84a2dc1a94846bb617e01b2c156a238597263fcc
Xie Jihui <xjh125470939@gmail.com>
2017-03-02 20:27:13 +0800

    Add unit tests to the DeleteCommand class

--- test/java/seedu/addressbook/util/TestUtil.java
+++ test/java/seedu/addressbook/util/TestUtil.java
@@ -7,17 +7,59 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
+import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
 import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList.DuplicatePersonException;
 import seedu.addressbook.data.tag.UniqueTagList;
 
 public class TestUtil {
+	/**
+     * Creates an address book containing the given persons.
+     */
+    public static AddressBook createAddressBook(Person... persons) {
+        AddressBook addressBook = new AddressBook();
+
+        for (Person person : persons) {
+            try {
+                addressBook.addPerson(person);
+            } catch (DuplicatePersonException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        return addressBook;
+    }
+    
+    /**
+     * Creates a list of persons.
+     */
+    public static List<ReadOnlyPerson> createList(Person...persons) {
+        List<ReadOnlyPerson> list = new ArrayList<ReadOnlyPerson>();
+
+        for (Person person : persons) {
+            list.add(person);
+        }
+
+        return list;
+    }
+    
+    /**
+     * Creates a copy of the original address book with the same entries
+     * of Persons and Tags. The Persons and Tags are not cloned.
+     */
+    public static AddressBook clone(AddressBook addressBook) {
+        return new AddressBook(addressBook.getAllPersons(), addressBook.getAllTags());
+    }
+	
     /**
      * Asserts whether the text in the two given files are the same. Ignores any
      * differences in line endings
