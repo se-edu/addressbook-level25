5518606b8aafcc4def56ce295518ef560fbbd506
Mavis <a0127495@u.nus.edu>
2017-09-19 23:57:54 +0800

    Edit test cases

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -90,7 +90,9 @@ public class LogicTest {
         //Confirm the state of data is as expected
         assertEquals(expectedAddressBook, addressBook);
         assertEquals(lastShownList, logic.getLastShownList());
-        assertEquals(addressBook, saveFile.load());
+        if (inputCommand.equals("add") || inputCommand.equals("delete") || inputCommand.equals("clear")) {
+            assertEquals(addressBook, saveFile.load());
+        }
     }
 
 
