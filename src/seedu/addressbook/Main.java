30f853e366472d221b16dd451fb18b4846ae8654
jo-lyn <e0007247@u.nus.edu>
2017-09-20 19:13:55 +0800

    Fix typo in window title

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -14,7 +14,7 @@ import seedu.addressbook.ui.Stoppable;
 public class Main extends Application implements Stoppable{
 
     /** Version info of the program. */
-    public static final String VERSION = "AddessBook Level 3 - Version 1.0";
+    public static final String VERSION = "AddressBook Level 3 - Version 1.0";
 
     private Gui gui;
 
